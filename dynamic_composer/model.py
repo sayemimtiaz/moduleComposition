c24463eb43f200b221ddc7ba60483c334204e5e7
astha99 <astha99@gmail.com>
2023-07-12 14:48:37 -0500

    shsh

--- dynamic_composer/model.py
+++ dynamic_composer/model.py
@@ -1,12 +1,22 @@
+import os
+
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout
 
 from util.data_util import get_mnist_data, get_fmnist_data, loadTensorFlowDataset
 
-# x_train, y_train, x_test, y_test, nb_classes = get_mnist_data()
-# x_train, y_train, x_test, y_test, nb_classes = get_fmnist_data()
-# x_train, y_train, x_test, y_test, nb_classes=loadTensorFlowDataset(datasetName='kmnist', hot=True)
-x_train, y_train, x_test, y_test, nb_classes=loadTensorFlowDataset(datasetName='emnist', hot=True)
+#model_scratch
+root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+model_name = os.path.join(root, 'h5', 'model_kmnist.h5')
+
+if 'fmnist' in model_name:
+    x_train, y_train, x_test, y_test, nb_classes = get_fmnist_data(hot=True)
+elif 'emnist' in model_name:
+    x_train, y_train, x_test, y_test, nb_classes = loadTensorFlowDataset(datasetName='emnist', hot=True)
+elif 'kmnist' in model_name:
+    x_train, y_train, x_test, y_test, nb_classes = loadTensorFlowDataset(datasetName='kmnist', hot=True)
+else:
+    x_train, y_train, x_test, y_test, nb_classes = get_mnist_data(hot=True)
 
 model = Sequential()
 
@@ -25,16 +35,16 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 
 print(model.summary())
 
-# epochs = 5
-#
-# history = model.fit(x_train,
-#                     y_train,
-#                     epochs=epochs,
-#                     batch_size=32,
-#                     verbose=2)
-# scores = model.evaluate(x_test, y_test, verbose=2)
-# print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+if 'scratch' not in model_name:
+    epochs = 5
+
+    history = model.fit(x_train,
+                        y_train,
+                        epochs=epochs,
+                        batch_size=32,
+                        verbose=2)
+    scores = model.evaluate(x_test, y_test, verbose=2)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 scores = model.predict(x_test)
-# model.save('h5/model_emnist.h5')
-model.save('h5/model_scratch.h5')
+model.save(model_name)
